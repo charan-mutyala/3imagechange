91366e3e9221a9f701bf35b0116c1ed348e5b2dc
charanmutyala <charanmutyala4@gmail.com>
2023-07-19 12:20:51 +0530

    hello

---
+++ Script.js
@@ -0,0 +1,9 @@
+function sunriseimage(){
+    document.getElementById("Sun").src="https://images.unsplash.com/photo-1614687345016-9183348d8f35?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3VucmlzZSUyMHNreXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+}
+function sunsetimage(){
+    document.getElementById("Sun").src="https://images.unsplash.com/photo-1599302592205-d7d683c83eea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJvcGljYWwlMjBzdW5zZXR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+}
+function nigthimage(){
+    document.getElementById("Sun").src="https://images.unsplash.com/photo-1590418606746-018840f9cd0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG5pZ2h0fGVufDB8fDB8fHww&w=1000&q=80"
+}
